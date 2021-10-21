ae2397c31a48979ff5ca97973b1df139fdd4d04b
stringimad <imadelfahssi@gmail.com>
2021-10-21 10:45:55 +0200

    Añade ej1 y pom

---
+++ src/main/java/ejercicios_bucles/Ej1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios_bucles;
+
+/**
+ *
+ * @author imad
+ */
+public class Ej1 {
+
+    public static void main(String[] args) {
+        int x = 1;
+
+        while (x <= 100) {
+            System.out.print(x);
+            System.out.print(" - ");
+            x = x + 1;
+        }
+
+    }
+}
